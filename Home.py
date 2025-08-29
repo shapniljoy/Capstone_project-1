b5d7ebd61016cf562d5cfe2856091be444907e07
shapniljoy <shapnildas12@gmail.com>
2025-08-29 14:28:17 +0600

    streamlit update -1

---
+++ Home.py
@@ -0,0 +1,9 @@
+import  streamlit as st
+
+st.set_page_config(
+    page_title='Hello'
+)
+
+st.write('Welcome to streamlit')
+
+st.sidebar.success('Select a demo above')
---
+++ pages/price_predictor.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(
+page_title='Demo1'
+)
+
+st.title('page1')
